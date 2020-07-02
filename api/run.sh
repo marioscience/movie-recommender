c6bedba77d20dae075206959a064e034df7b50f0
Mario Matos <mariomatos889@gmail.com>
2020-07-02 16:09:55 -0500

    added project skeleton

---
+++ api/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export FLASK_APP=src/recommender-api.py
+if [[ $1 != "-production" ]]
+then
+        export FLASK_ENV=development
+else
+        export FLASK_ENV=production
+fi
+
+flask run
