693e1ca0db5c38bed319f8b3f8bc25072ea8d07a
Mario Matos <mariomatos889@gmail.com>
2020-07-06 20:23:38 -0500

    added logic to add OMDB api key

--- api/run.sh
+++ api/run.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 export FLASK_APP=src/recommender-api.py
-if [ $1 != '-production' ]
+if [ "$1" != '-production' ]
 then
 	export FLASK_ENV=development
 else
 	export FLASK_ENV=production
+fi
+
+if [ -f OMDB_API_KEY ]
+then
+	source OMDB_API_KEY
+else
+	echo "No OMDB_API_KEY file found. Set 'export OMDB_API_KEY=Your_Key_Here' in movie-recommender/api/OMDB_API_KEY file. Get key: https://www.omdbapi.com/apikey.aspx"
 fi
 
 flask run --host 0.0.0.0
