109fa2a4d54ba15eb57148702ac3cfbf3c0d6f95
Mario Matos <mariomatos889@gmail.com>
2020-07-08 22:18:52 -0500

    updated stylings

--- client/src/components/404/404.js
+++ client/src/components/404/404.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Error(props) {
     return (
-        <h1>Oops, That page is not found here :(</h1>
+        <h1 style={{color: 'white'}}>Oops, That page is not http://localhost:5000/api/rate/10/680found here :(</h1>
     );
 }
 
