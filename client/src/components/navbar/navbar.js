21310b7e86b6880eb3f6e0a4fd603827ec360b45
Mario Matos <mariomatos889@gmail.com>
2020-07-08 22:39:45 -0500

    adding link to home for navbar

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+import {Link} from "react-router-dom";
 
 import './navbar.css';
 
 class Navbar extends React.Component {
     render() {
         return (<div id="navbar">
-                    <div className="header"><h1>Movie Recommender</h1></div>
+                    <div className="header"><Link to={`/`}><h1 className="header-title">Movie Recommender</h1></Link></div>
                     <div className="links"><ul></ul></div>
                     <div className="user-box"></div>
                 </div>);
